9359444fd06e4a6c6897ad93deac8b21f589a1d6
Phil Bennett <phil@happyaccidents.me>
2013-02-20 21:41:55 +0000

    Index page

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
 <body>
     <p>Full descriptions and code coming soon...</p>
     <ul>
-        <li><a href="/benchmark/1.php">Benchamrk 1</a> - Auto resolution of object and dependencies. (Aliasing Interfaces to Concretes)</li>
-        <li><a href="/benchmark/2.php">Benchamrk 2</a> - Auto resolution of object and dependencies. (Register all objects with container)</li>
-        <li><a href="/benchmark/3.php">Benchamrk 3</a> - Factory closure resolution. (Expecting this to be pretty much the same for each)</li>
-        <li><a href="/benchmark/4.php">Benchamrk 4</a> - Constructor injection with defined arguments.</li>
-        <li><a href="/benchmark/5.php">Benchamrk 5</a> - Setter injection with defined setter methods and arguments.</li>
+        <li><a href="/benchmark/1.php">Benchmark 1</a> - Auto resolution of object and dependencies. (Aliasing Interfaces to Concretes)</li>
+        <li><a href="/benchmark/2.php">Benchmark 2</a> - Auto resolution of object and dependencies. (Register all objects with container)</li>
+        <li><a href="/benchmark/3.php">Benchmark 3</a> - Factory closure resolution. (Expecting this to be pretty much the same for each)</li>
+        <li><a href="/benchmark/4.php">Benchmark 4</a> - Constructor injection with defined arguments.</li>
+        <li><a href="/benchmark/5.php">Benchmark 5</a> - Setter injection with defined setter methods and arguments.</li>
     </ul>
     <h3>TODO</h3>
     <ul>
