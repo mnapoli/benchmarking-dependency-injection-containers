9d0934cda5853e21a70221d31540e9b1f7813faf
Phil Bennett <phil@happyaccidents.me>
2013-02-20 21:07:28 +0000

    Benchmark 5

--- benchmarks/5.php
+++ benchmarks/5.php
@@ -37,32 +37,6 @@ $bm->end('benchmark5', 'orno');
 unset($orno);
 unset($foo);
 
-// Orno\Di
-$bm->start('benchmark5', 'orno');
-$orno = new Orno\Di\Container;
-$orno->register('Benchmark\Stubs\Bart');
-$orno->register('Benchmark\Stubs\Bam')->withMethodCall('setBart', [$orno->resolve('Benchmark\Stubs\Bart')]);
-$orno->register('Benchmark\Stubs\Baz')->withMethodCall('setBam', [$orno->resolve('Benchmark\Stubs\Bam')]);
-$orno->register('Benchmark\Stubs\Bar')->withMethodCall('setBaz', [$orno->resolve('Benchmark\Stubs\Baz')]);
-$orno->register('Benchmark\Stubs\Foo')->withMethodCall('setBar', [$orno->resolve('Benchmark\Stubs\Bar')]);
-$foo = $orno->resolve('Benchmark\Stubs\Foo');
-$bm->end('benchmark5', 'orno');
-unset($orno);
-unset($foo);
-
-// Symfony\DependencyInjection
-$bm->start('benchmark5', 'symfony');
-$symfony = new Symfony\Component\DependencyInjection\ContainerBuilder;
-$symfony->register('foo', 'Benchmark\Stubs\Foo')->addMethodCall('setBar', [new Symfony\Component\DependencyInjection\Reference('bar')]);
-$symfony->register('bar', 'Benchmark\Stubs\Bar')->addMethodCall('setBaz', [new Symfony\Component\DependencyInjection\Reference('baz')]);
-$symfony->register('baz', 'Benchmark\Stubs\Baz')->addMethodCall('setBam', [new Symfony\Component\DependencyInjection\Reference('bam')]);
-$symfony->register('bam', 'Benchmark\Stubs\Bam')->addMethodCall('setBart', [new Symfony\Component\DependencyInjection\Reference('bart')]);
-$symfony->register('bart', 'Benchmark\Stubs\Bart');
-$foo = $symfony->get('foo');
-$bm->end('benchmark5', 'symfony');
-unset($symfony);
-unset($foo);
-
 ?>
 
 
@@ -83,9 +57,9 @@ unset($foo);
     google.setOnLoadCallback(drawChart);
     function drawChart() {
         var data = google.visualization.arrayToDataTable([
-            ['Component', 'Run 1', 'Run 2'],
-            ['Symfony\\DependencyInjection', <?= $bm->getBenchmarkData('benchmark5')['symfony']['time'][0] ?>, <?= $bm->getBenchmarkData('benchmark5')['symfony']['time'][1] ?>],
-            ['Orno\\Di', <?= $bm->getBenchmarkData('benchmark5')['orno']['time'][0] ?>, <?= $bm->getBenchmarkData('benchmark5')['orno']['time'][1] ?>]
+            ['Component', 'Time Taken'],
+            ['Symfony\\DependencyInjection', <?= $bm->getBenchmarkData('benchmark5')['symfony']['time'][0] ?> ?>],
+            ['Orno\\Di', <?= $bm->getBenchmarkData('benchmark5')['orno']['time'][0] ?> ?>]
         ]);
 
         var options = {
