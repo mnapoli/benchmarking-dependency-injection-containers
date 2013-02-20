67804e186ed927298131904b919bdb1385682c19
Phil Bennett <phil@happyaccidents.me>
2013-02-20 21:40:24 +0000

    Index page

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Benchmark 4</title>
 
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+</head>
+<body>
+    <p>Full descriptions and code coming soon...</p>
+    <ul>
+        <li><a href="/benchmark/1.php">Benchamrk 1</a> - Auto resolution of object and dependencies. (Aliasing Interfaces to Concretes)</li>
+        <li><a href="/benchmark/2.php">Benchamrk 2</a> - Auto resolution of object and dependencies. (Register all objects with container)</li>
+        <li><a href="/benchmark/3.php">Benchamrk 3</a> - Factory closure resolution. (Expecting this to be pretty much the same for each)</li>
+        <li><a href="/benchmark/4.php">Benchamrk 4</a> - Constructor injection with defined arguments.</li>
+        <li><a href="/benchmark/5.php">Benchamrk 5</a> - Setter injection with defined setter methods and arguments.</li>
+    </ul>
+    <h3>TODO</h3>
+    <ul>
+        <li>Add Zend\Di to BM 3, BM 4 &amp; BM 5</li>
+    </ul>
+</body>
+</html>
